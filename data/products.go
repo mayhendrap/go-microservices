4b86c42f4568f5090d747673bb28092a3c386f5d
mayhendrap <mayhendra.p10@gmail.com>
2022-07-11 02:54:59 +0700

    create (create read update) operation for products

--- data/products.go
+++ data/products.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"time"
 )
@@ -19,6 +20,13 @@ type Product struct {
 
 type Products []*Product
 
+var ErrProductNotFound = fmt.Errorf("Product not found")
+
+func (p *Product) FromJson(r io.Reader) error {
+	e := json.NewDecoder(r)
+	return e.Decode(p)
+}
+
 func (p *Products) ToJSON(w io.Writer) error {
 	e := json.NewEncoder(w)
 	return e.Encode(p)
@@ -26,6 +34,37 @@ func (p *Products) ToJSON(w io.Writer) error {
 
 func GetProducts() Products {
 	return productList
+}
+
+func AddProduct(p *Product) {
+	p.ID = getNextID()
+	productList = append(productList, p)
+}
+
+func UpdateProduct(id int, prod *Product) error {
+	fp, pos, err := findProduct(id)
+	if err != nil {
+		return err
+	}
+
+	prod.ID = fp.ID
+	productList[pos] = prod
+	return nil
+}
+
+func findProduct(id int) (*Product, int, error) {
+	for i, p := range productList {
+		if p.ID == id {
+			return p, i, nil
+		}
+	}
+
+	return nil, -1, ErrProductNotFound
+}
+
+func getNextID() int {
+	lp := productList[len(productList)-1]
+	return lp.ID + 1
 }
 
 var productList = []*Product{
--- handlers/products.go
+++ handlers/products.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 
 	"github.com/mayhendrap/go-microservices/data"
 )
@@ -16,19 +18,84 @@ func NewProducts(l *log.Logger) *Products {
 }
 
 func (p *Products) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
+	switch r.Method {
+	case http.MethodGet:
 		p.getProducts(w, r)
 		return
-	}
+	case http.MethodPost:
+		p.addProduct(w, r)
+		return
+	case http.MethodPut:
+		regexp := regexp.MustCompile(`/([0-9]+)`)
+		g := regexp.FindAllStringSubmatch(r.URL.Path, -1)
 
-	// catch all
-	w.WriteHeader(http.StatusMethodNotAllowed)
+		p.l.Println("GOT REGEX PATH :", g[0][1])
+
+		if len(g) != 1 {
+			p.l.Println("Invalid URI more than one id")
+			http.Error(w, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		if len(g[0]) != 2 {
+			p.l.Println("Invalid URI more than one capture group")
+			http.Error(w, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		id, err := strconv.Atoi(g[0][1])
+		if err != nil {
+			p.l.Println("Invalid URI unable to convert to number", g[0][1])
+			http.Error(w, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		err = p.updateProduct(id, w, r)
+		if err == data.ErrProductNotFound {
+			http.Error(w, "Product not found", http.StatusNotFound)
+			return
+		}
+
+		if err != nil {
+			http.Error(w, "Product not found", http.StatusInternalServerError)
+			return
+		}
+		return
+	default:
+		// catch all
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
 }
 
 func (p *Products) getProducts(w http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle GET Products")
 	lp := data.GetProducts()
 	err := lp.ToJSON(w)
 	if err != nil {
 		http.Error(w, "Unable to marshal json", http.StatusInternalServerError)
 	}
+}
+
+func (p *Products) addProduct(w http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle POST Product")
+
+	prod := &data.Product{}
+	err := prod.FromJson(r.Body)
+	if err != nil {
+		http.Error(w, "Unable to unmarshal json", http.StatusBadRequest)
+	}
+
+	data.AddProduct(prod)
+}
+
+func (p Products) updateProduct(id int, w http.ResponseWriter, r *http.Request) error {
+	p.l.Println("Handle PUT Product")
+
+	prod := &data.Product{}
+	err := prod.FromJson(r.Body)
+	if err != nil {
+		http.Error(w, "Unable to unmarshal json", http.StatusBadRequest)
+	}
+
+	return data.UpdateProduct(id, prod)
 }
